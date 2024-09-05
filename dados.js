2364ef1e7bd9e4459c801301eb237cd4d9f4efa2
Matheus Lima <matheuslimacarneirors@gmail.com>
2024-09-05 16:34:37 -0300

    Criando a base de Dados com Js

---
+++ dados.js
@@ -0,0 +1,43 @@
+let personagens =[
+    {
+        nome: "Jake Peralta",
+        descricao: "Jake Peralta, interpretado por Andy Samberg, é um dos personagens mais queridos de Brooklyn Nine-Nine. Detetive da 99ª delegacia do Brooklyn, Jake é conhecido por sua personalidade divertida, brincalhona e um pouco infantil. Apesar de sua aparência despreocupada, ele é um policial talentoso com uma taxa de resolução de casos impressionante. A jornada de Jake ao longo da série acompanha sua evolução pessoal e profissional, enquanto ele balanceia sua paixão por resolver crimes com a necessidade de amadurecer e se tornar um líder mais responsável. Sua dinâmica com a parceira Amy Santiago e a relação com o Capitão Holt são alguns dos destaques da série.",
+        link : "https://brooklyn99.fandom.com/wiki/Jake_Peralta#:~:text=Jacob%20Jeffrey%20Sherlock%20%22Jake%22%20Peralta%20(portrayed%20by%20Andy%20Samberg)%20is",
+        Ator: "Andy_Samberg",
+        franquia: "Brooklyn Nine-Nine",
+        anoLancamento: 2013
+    },
+    {
+        nome: "Arthur Morgan",
+        descricao: "Arthur Morgan é o protagonista de Red Dead Redemption 2, um jogo de mundo aberto ambientado no Velho Oeste. Um fora da lei complexo, Arthur luta para sobreviver e encontrar seu lugar em um mundo em constante mudança. Sua jornada é marcada por dilemas morais e pela lealdade ao seu bando, a gangue de Dutch van der Linde.",
+        link: "https://reddead.fandom.com/pt-br/wiki/Arthur_Morgan#:~:text=Arthur%20Morgan%20%C3%A9%20um%20personagem%20da%20s%C3%A9rie%20Red%20Dead%20e",
+        Ator: "Roger Clark (captura de movimentos e voz)",
+        franquia: "Red Dead Redemption 2",
+        anoLancamento: 2018
+    },
+    {
+        nome: "Thorfinn Karlsefni",
+        descricao: "Thorfinn Karlsefni é o protagonista do anime Vinland Saga. Um jovem viking em busca de vingança pela morte de seu pai, Thorfinn embarca em uma jornada épica que o leva a descobrir novos mundos e a questionar seus valores. Sua história é marcada por batalhas, traição e a busca por um lugar para chamar de lar.",
+        link: "https://vinlandsaga.fandom.com/wiki/Thorfinn_Karlsefni",
+        Ator: "Yuuki Ono (dublador japonês)", 
+        franquia: "Vinland Saga",
+        anoLancamento: 2019
+    },
+    {
+        nome: "Timmy Turner",
+        descricao: "Timmy Turner é um menino de 10 anos com uma vida familiar difícil. Para escapar de seus problemas, ele conta com a ajuda de seus padrinhos mágicos, Cosmo e Wanda, que lhe concedem desejos. Timmy é conhecido por seus planos mirabolantes, que geralmente dão errado, e por sua rivalidade com Vicky, sua babá.",
+        link: "https://fairygodparents.fandom.com/wiki/Timmy_Turner",
+        Ator: "Drake Bell (voz original)" ,
+        franquia: "The Fairly OddParents",
+        anoLancamento: 2001
+    },
+    {
+        nome: "Bob Esponja",
+        descricao: "Bob Esponja é uma esponja do mar otimista e entusiasta que vive na cidade submarina de Bikini Bottom. Trabalha como cozinheiro no restaurante fast-food Crustáceo Cascudo e é conhecido por sua amizade com Patrick Estrela e sua paixão por hambúrgueres de siri.",
+        link: "https://spongebob.fandom.com/wiki/SpongeBob_SquarePants",
+        Ator: "Tom Kenny (voz original)", 
+        franquia: "SpongeBob SquarePants",
+        anoLancamento: 1999
+    }
+]
+
