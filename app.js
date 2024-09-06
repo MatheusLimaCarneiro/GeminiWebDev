3d692876dfb1c8cb9df3783d2cfd1753bcf5a5f4
Matheus Lima <matheuslimacarneirors@gmail.com>
2024-09-06 18:32:07 -0300

    finalizando projeto

--- app.js
+++ app.js
@@ -1,26 +1,79 @@
 function pesquisar() {
-    // Seleciona o elemento HTML com o ID "resultados-pesquisa" e armazena na variável section.
-    let section = document.getElementById("resultados-pesquisa");
-  
-    // Inicializa uma string vazia para armazenar os resultados da pesquisa.
-    let resultados = "";
-  
-    // Itera sobre cada personagem na lista de personagens.
-    for (personagem of personagens) {
-      // Constrói o HTML para cada item de resultado, incluindo a imagem, nome, descrição e link.
-      // A string é concatenada à variável resultados.
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <img src="${personagem.imagem}" alt="Jake Peralta">
-            <a href="#" target="_blank">${personagem.nome}</a>
-          </h2>
-          <p class="descricao-meta">${personagem.descricao}</p>
-          <a href="${personagem.link}" target="_blank">Mais Informações</a>
-        </div>
-      `;
-    }
-  
-    // Atribui o conteúdo HTML completo da variável resultados ao elemento section.
-    section.innerHTML = resultados;
+  // Seleciona o elemento HTML com o ID "resultados-pesquisa" e armazena na variável section.
+  let section = document.getElementById("resultados-pesquisa");
+
+  // Obtém o valor digitado no campo de pesquisa.
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  // Verifica se o campo de pesquisa está vazio.
+  if (!campoPesquisa) {
+    // Se estiver vazio, exibe uma mensagem de erro.
+    section.innerHTML = "<p> Nenhum Personagem foi encontrado<p>";
+    return;
   }
+
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa.
+  let resultados = "";
+
+  // Itera sobre cada personagem na lista de personagens.
+  for (personagem of personagens) {
+    // Divide o nome do personagem e a pesquisa em palavras para comparação.
+    const nomePalavras = personagem.nome.toLowerCase().split(" ");
+    const pesquisaPalavras = campoPesquisa.toLowerCase().split(" ");
+
+    // Verifica se todas as palavras da pesquisa estão presentes no nome do personagem.
+    let todasPalavrasEncontradas = true;
+    for (const palavraPesquisa of pesquisaPalavras) {
+      if (!nomePalavras.includes(palavraPesquisa)) {
+        todasPalavrasEncontradas = false;
+        break;
+      }
+    }
+
+    // Verifica se todas as palavras da pesquisa estão presentes na descrição do personagem.
+    const descricaoPalavras = personagem.descricao.toLowerCase().split(" ")
+    let palavrasDescricaoEncontradas = true;
+    for( const palavraPesquisa of pesquisaPalavras){
+      if(!descricaoPalavras.includes(palavraPesquisa)){
+        palavrasDescricaoEncontradas = false;
+        break;
+      }
+    }
+
+    // Verifica se alguma das palavras da pesquisa está presente nas tags do personagem.
+    tags = personagem.tags.toLowerCase().split(" ");
+    let tagsEncontradas = false;
+    for (const palavraPesquisa of pesquisaPalavras) {
+      if (tags.includes(palavraPesquisa)) {
+        tagsEncontradas = true;
+        break;
+      }
+    }
+
+    // Se alguma das condições acima for verdadeira, o personagem é adicionado aos resultados.
+    if (todasPalavrasEncontradas || palavrasDescricaoEncontradas || tagsEncontradas) {
+      // Cria um elemento HTML para exibir os detalhes do personagem.
+      resultados +=  `
+  <div class="item-resultado">
+    <h2>
+      <img src="${personagem.imagem}" alt="${personagem.nome}">
+      <a href="#" target="_blank">${personagem.nome}</a>
+    </h2>
+    <p class="descricao-meta">${personagem.descricao}</p>
+    <p><strong>Ator:</strong> ${personagem.Ator}</p>
+    <p><strong>Franquia:</strong> ${personagem.franquia}</p>
+    <p><strong>Ano de Lançamento:</strong> ${personagem.anoLancamento}</p>
+    <a href="${personagem.link}" target="_blank">Mais Informações</a>
+  </div>
+  `;
+}
+  }
+
+  // Se nenhum resultado foi encontrado, exibe uma mensagem de erro.
+  if(!resultados){
+    resultados = "<p>Nada foi encontrado.Você precisa digitar o nome de um personagem!!<p>";
+  }
+
+  // Atribui o conteúdo HTML completo da variável resultados ao elemento section.
+  section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -5,8 +5,9 @@ let personagens =[
         descricao: "Jake Peralta, interpretado por Andy Samberg, é um dos personagens mais queridos de Brooklyn Nine-Nine. Detetive da 99ª delegacia do Brooklyn, Jake é conhecido por sua personalidade divertida, brincalhona e um pouco infantil. Apesar de sua aparência despreocupada, ele é um policial talentoso com uma taxa de resolução de casos impressionante. A jornada de Jake ao longo da série acompanha sua evolução pessoal e profissional, enquanto ele balanceia sua paixão por resolver crimes com a necessidade de amadurecer e se tornar um líder mais responsável. Sua dinâmica com a parceira Amy Santiago e a relação com o Capitão Holt são alguns dos destaques da série.",
         link : "https://brooklyn99.fandom.com/wiki/Jake_Peralta#:~:text=Jacob%20Jeffrey%20Sherlock%20%22Jake%22%20Peralta%20(portrayed%20by%20Andy%20Samberg)%20is",
         Ator: "Andy_Samberg",
-        franquia: "Brooklyn Nine-Nine",
-        anoLancamento: 2013
+        franquia: "Brooklyn Nine-Nine série",
+        anoLancamento: 2013,
+        tags: "comédia policial parceira líder evolução Brooklyn Nine-Nine"
     },
     {
        imagem: "https://th.bing.com/th/id/R.c40d508127e51adfdd7153cf998f010c?rik=m19alXForfeC%2bw&pid=ImgRaw&r=0",
@@ -15,7 +16,8 @@ let personagens =[
         link: "https://reddead.fandom.com/pt-br/wiki/Arthur_Morgan#:~:text=Arthur%20Morgan%20%C3%A9%20um%20personagem%20da%20s%C3%A9rie%20Red%20Dead%20e",
         Ator: "Roger Clark (captura de movimentos e voz)",
         franquia: "Red Dead Redemption 2",
-        anoLancamento: 2018
+        anoLancamento: 2018,
+        tags: "faroeste fora da lei gangue dilema moral Red Dead Redemption 2"
     },
     {
         imagem: "https://th.bing.com/th/id/OIP.iw3uRVUg1rnhbr6G7tUOYAHaEK?rs=1&pid=ImgDetMain",
@@ -24,7 +26,8 @@ let personagens =[
         link: "https://vinlandsaga.fandom.com/wiki/Thorfinn_Karlsefni",
         Ator: "Yuuki Ono (dublador japonês)", 
         franquia: "Vinland Saga",
-        anoLancamento: 2019
+        anoLancamento: 2019,
+        tags: "anime ação viking vingança jornada Vinland Saga"
     },
     {
         imagem: "https://wallpaperaccess.com/full/1918550.jpg",
@@ -33,7 +36,8 @@ let personagens =[
         link: "https://fairlyoddparents.fandom.com/wiki/Timmy_Turner",
         Ator: "Drake Bell (voz original)" ,
         franquia: "The Fairly OddParents",
-        anoLancamento: 2001
+        anoLancamento: 2001,
+        tags: "animação infantil magia família desejo The Fairly OddParents Padrinhos Magicos "
     },
     {
         imagem: "https://holamifan.com/wp-content/uploads/2020/08/Bob-Esponja2.jpg",
@@ -42,7 +46,72 @@ let personagens =[
         link: "https://spongebob.fandom.com/wiki/SpongeBob_SquarePants",
         Ator: "Tom Kenny (voz original)", 
         franquia: "SpongeBob SquarePants",
-        anoLancamento: 1999
-    }
+        anoLancamento: 1999,
+        tags:"animação comédia mar amizade comida Bob Esponja SpongeBob SquarePants"
+    },
+    {
+        imagem: "https://th.bing.com/th/id/OIP.xfvQG2jdbG8BW0YA1_8Q_QAAAA?w=438&h=438&rs=1&pid=ImgDetMain",
+        nome: "Deadpool",
+        descricao: "Deadpool é um personagem fictício do universo Marvel, conhecido por seu humor sarcástico e irreverente. Seu nome verdadeiro é Wade Wilson, um ex-militar e mercenário que, após ser diagnosticado com câncer terminal, se submete a um experimento científico que lhe concede poderes de cura acelerada, mas também o deixa com cicatrizes pelo corpo1.Deadpool é famoso por quebrar a quarta parede, ou seja, ele frequentemente fala diretamente com o público, algo raro entre os personagens de quadrinhos1. Ele é interpretado por Ryan Reynolds nos filmes, que capturam bem seu estilo único de humor e ação.",
+        link: "https://marvel.fandom.com/wiki/Deadpool",
+        Ator: "Ryan Reynolds",
+        franquia: "Marvel Comics",
+        anoLancamento: 2016,
+        tags: "super-herói comédia ação mercenário Marvel"
+      },
+    
+      {
+        imagem: "https://i.pinimg.com/originals/15/db/ea/15dbea676d850fb0e0d7b689a3e28172.jpg",
+        nome: "Homem de Ferro",
+        descricao: "O Homem de Ferro, cujo nome verdadeiro é Tony Stark, é um personagem icônico da Marvel Comics. Criado por Stan Lee, Larry Lieber, Don Heck e Jack Kirby, ele fez sua primeira aparição em 19631. Tony Stark é um bilionário, inventor brilhante e CEO das Indústrias Stark. Após ser sequestrado por terroristas, ele constrói uma armadura de alta tecnologia para escapar e, posteriormente, usa essa armadura para combater o crime como o Homem de Ferro.Nos filmes, Tony Stark é interpretado por Robert Downey Jr, que trouxe uma nova dimensão ao personagem com seu carisma e humor. A armadura do Homem de Ferro é equipada com diversas tecnologias avançadas, incluindo voo, força sobre-humana e uma variedade de armas",
+        link: "https://marvel.fandom.com/wiki/Iron_Man",
+        Ator: "Robert Downey Jr.",
+        franquia: "Marvel Cinematic Universe",
+        anoLancamento: 2008,
+        tags: "super-herói tecnologia bilionário invento Marvel"
+      },
+    
+      {
+        imagem: "https://th.bing.com/th/id/OIP.3qv4XKfBp1zGQ0PzO9XZDgAAAA?rs=1&pid=ImgDetMain",
+        nome: "Homem-Aranha (Tobey Maguire)",
+        descricao: "O Homem-Aranha, também conhecido como Peter Parker, é um dos super-heróis mais icônicos da Marvel Comics. Criado por Stan Lee e Steve Ditko, ele fez sua primeira aparição em 1962 na revista " +"Amazing Fantasy #15" +". Peter Parker é um jovem estudante que ganha habilidades sobre-humanas após ser mordido por uma aranha radioativa. Entre seus poderes estão a capacidade de escalar paredes, força e agilidade aumentadas, e um “sentido aranha” que o alerta sobre perigos iminentes. Após a morte de seu tio Ben, Peter adota o lema “com grandes poderes vêm grandes responsabilidades” e decide usar suas habilidades para combater o crime em Nova York. O Homem-Aranha é conhecido por seu humor espirituoso e sua habilidade de equilibrar a vida de super-herói com os desafios pessoais e acadêmicos de ser um jovem adulto.",
+        link: "https://marvel.fandom.com/wiki/Spider-Man_(Earth-616)",
+        Ator: "Tobey Maguire",
+        franquia: "Homem-Aranha (Trilogia de Sam Raimi)",
+        anoLancamento: 2002,
+        tags: "super-herói adolescente aranha Marvel"
+      },
+      {
+        imagem: "https://64.media.tumblr.com/12e2e32a118019aa2c18dbdcc2385767/d6cb4b6cea1fc625-63/s1280x1920/6629277b3818ab62504aac3c3a36565b15ea0716.jpg",
+        nome: "Homem-Aranha (Andrew Garfield)",
+        descricao: "Andrew Garfield interpretou Peter Parker em uma nova trilogia do Homem-Aranha, explorando um lado mais jovem e rebelde do personagem.",
+        link: "https://marvel.fandom.com/wiki/Spider-Man_(Earth-616)", 
+        Ator: "Andrew Garfield",
+        franquia: "O Espetacular Homem-Aranha",
+        anoLancamento: 2012,
+        tags: "super-herói adolescente aranha Marvel"
+      },
+    
+      {
+        imagem: "https://th.bing.com/th/id/OIP.RX8re1ScGF-tZafwbjpkCAAAAA?w=474&h=474&rs=1&pid=ImgDetMain",
+        nome: "Homem-Aranha (Tom Holland)",
+        descricao: "Tom Holland trouxe uma nova perspectiva ao papel de Peter Parker, integrando o personagem ao Universo Cinematográfico Marvel.",
+        link: "https://marvel.fandom.com/wiki/Spider-Man_(Earth-616)", 
+        Ator: "Tom Holland",
+        franquia: "Marvel Cinematic Universe",
+        anoLancamento: 2016,
+        tags: "super-herói adolescente aranha Marvel MCU"
+      },
+    
+      {
+        imagem: "https://th.bing.com/th/id/R.0538bdb1c303250964d04be8e9821342?rik=aRpMudWN2mxw%2fQ&pid=ImgRaw&r=0",
+        nome: "Sonic (2020)",
+        descricao: "Ouriço azul super rápido que luta contra o Dr. Eggman para salvar o mundo.",
+        link: "https://sonic.fandom.com/wiki/Sonic_the_Hedgehog",
+        Ator: "Ben Schwartz (voz)",
+        franquia: "Sonic the Hedgehog (filme 2020)",
+        anoLancamento: 2020,
+        tags: "videogame ouriço velocidade ação"
+      }
 ]
 
